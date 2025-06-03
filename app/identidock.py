f57fe74dd1eb9bee3dea9980b139c44705aeda36
Levardinio <levandovskiy1993@gmail.com>
2025-06-03 13:32:05 +0300

    Updated code

--- app/identidock.py
+++ app/identidock.py
@@ -1,10 +1,50 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, Response, request
+import requests 
+import hashlib
+import redis
 
-@app.route('/')
-def hello_world():
-    return 'Hello world! \n'
+app = Flask(__name__)
+# ❌ Было: StirictRedis, prort
+# ✅ Стало: StrictRedis, port
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+
+default_name = "Specter Center"
+salt = "UNIQUE_SALT_123"  # можно любое слово, главное — стабильное
+
+@app.route('/', methods=['GET', 'POST'])
+def mainpage():
+    name = default_name
+    if request.method == 'POST':
+        name = request.form.get('name', default_name)
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
+    header = "<html><head><title>Identidock</title></head><body>"
+    body = f'''
+        <form method="POST">
+            Hello <input type="text" name="name" value="{name}">
+            <input type="submit" value="submit">
+        </form>
+        <p>You look like a:
+        <img src="/avatar/{name_hash}"/>
+    '''
+    footer = '</body></html>'
+
+    return header + body + footer
+
+@app.route('/avatar/<name>')
+def get_identicon(name):
+    image = cache.get(name)
+    if image is None:
+        print("cache miss", flush=True)
+        r = requests.get(f'http://dnmonster:8080/monster/{name}?size=80')
+        image = r.content
+        cache.set(name, image)
+
+    return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
 
--- dockerfile
+++ dockerfile
@@ -1,6 +1,22 @@
-FROM python:3.13
+# FROM python:3.13.1
+# RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
+# RUN pip install Flask==3.1.0 uWSGI==2.0.28 requests==2.32.3
+# WORKDIR /app
+# COPY app /app/
+# COPY cmd.sh /
+# RUN chmod a+x /cmd.sh
+# EXPOSE 9090 9191
+# USER uwsgi
+# CMD [ "/cmd.sh"]
 
-RUN pip install Flask==3.1.1
+FROM python:3.13.1
+RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
+RUN pip install Flask==3.1.0 uWSGI==2.0.28 requests==2.32.3 redis==6.2.0
 WORKDIR /app
 COPY app /app/
-CMD [ "python", "identidock.py" ]
+COPY cmd.sh /cmd.sh
+RUN chmod a+x /cmd.sh
+EXPOSE 5000 9090
+USER uwsgi
+CMD ["/cmd.sh"]
+
